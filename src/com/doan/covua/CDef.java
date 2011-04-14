624e786dafcbb34c9547232770693918d5db4570
lephuong06t3 <lephuong06t3@gmail.com>
2011-04-14 16:04:00 +0000

    + add new defines

---
+++ src/com/doan/covua/CDef.java
@@ -0,0 +1,12 @@
+package com.doan.covua;
+
+public final class CDef {
+
+	//define the screen
+	public static final int SCR_W = 320;
+	public static final int SCR_H = 480;
+	//define the chess table
+	public static final int cellSize = 35;
+	public static final int xOrg		= 20;
+	public static final int yOrg		= 50;
+}
