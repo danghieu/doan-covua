2ac5c6db2ca7ec82a2e7be2b12d5e12bbb67bea7
lephuong06t3 <lephuong06t3@gmail.com>
2011-04-14 04:23:54 +0000

    + add new About

---
+++ src/com/doan/covua/states/About.java
@@ -0,0 +1,18 @@
+package com.doan.covua.states;
+
+
+
+import com.doan.covua.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+	}
+}
