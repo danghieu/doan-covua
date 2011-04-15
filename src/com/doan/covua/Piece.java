565c821982b423b5bcfa8e18eb74496fd36fc445
lephuong06t3 <lephuong06t3@gmail.com>
2011-04-15 13:25:16 +0000

    - draw piece on table

--- src/com/doan/covua/Piece.java
+++ src/com/doan/covua/Piece.java
@@ -2,21 +2,22 @@ package com.doan.covua;
 
 public final class Piece {
 
-	final static byte wpawn		= 1;
-	final static byte bpawn		= 2;
-	final static byte wknight	= 3;
-	final static byte bknight	= 4;
-	final static byte wking		= 5;
-	final static byte bking		= 6;
-	final static byte wrook		= 7;
-	final static byte brook		= 8;
-	final static byte wqueen	= 9;
-	final static byte bqueen	= 10;
-	final static byte wbishop	= 11;
-	final static byte bbishop	= 12;
-	final static byte npiece	= 13;
-	final static long INF		= 1000000;
-	final static int MOVE_BUFF	= 60;
-	final static int LENGTH		= 144;
+	public final static byte wpawn		= 1;
+	public final static byte bpawn		= 2;
+	public final static byte wknight	= 3;
+	public final static byte bknight	= 4;
+	public final static byte wking		= 5;
+	public final static byte bking		= 6;
+	public final static byte wrook		= 7;
+	public final static byte brook		= 8;
+	public final static byte wqueen	= 9;
+	public final static byte bqueen	= 10;
+	public final static byte wbishop	= 11;
+	public final static byte bbishop	= 12;
+	public final static byte npiece	= 13;
+	public final static long INF		= 1000000;
+	public final static int MOVE_BUFF	= 60;
+	public final static int LENGTH		= 144;
+	
 	
 }
