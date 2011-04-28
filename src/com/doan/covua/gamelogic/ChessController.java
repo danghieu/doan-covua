7429cd02ac7d6ba85ce9d16d958a579bda9f1cf2
lephuong06t3 <lephuong06t3@gmail.com>
2011-04-28 08:30:26 +0000

    - add up the class Game into ChessController

--- src/com/doan/covua/gamelogic/ChessController.java
+++ src/com/doan/covua/gamelogic/ChessController.java
@@ -1,29 +1,30 @@
 package com.doan.covua.gamelogic;
 
-import android.util.Log;
-
 import com.doan.covua.GUIInterface;
-
+import com.doan.covua.gamelogic.Position;;
 public class ChessController {
 
 	PgnToken.PgnTokenReceiver gameTextListener;
 	Game game;
 	GUIInterface gui;
+	GameTree tree;
+	
 	public ChessController(GUIInterface gui, PgnToken.PgnTokenReceiver gamTextListener){
 		this.gui = gui;
 		this.gameTextListener = gamTextListener;
 	}
 	public final void newGame(){
-		game = new Game(gameTextListener);
+		tree = new GameTree(gameTextListener);
 	}
 	public final void startGame() {
 		updateUI();
 	}
 	private final void updateUI(){
-		try{
-		gui.setPosition(game.currPos(), null, null);
-		}catch(Exception e){
-			Log.v("update UI...","" + e);
-		}
+		
+		gui.setPosition(currPos(), null, null);
+	}
+	/** get current position */
+	final Position currPos(){
+		return tree.currentPos;
 	}
 }
