a681e82ffeee34d2b1c36ea0fcabe13dbc2759cc
lephuong06t3@gmail.com <lephuong06t3@gmail.com@9bc38225-aa60-16d1-9924-7d5f6a9606a2>
2011-05-01 04:36:54 +0000

    + make a moves

--- trunk/src/com/doan/covua/ChessBoard.java
+++ trunk/src/com/doan/covua/ChessBoard.java
@@ -2,9 +2,11 @@ package com.doan.covua;
 
 
 import com.doan.covua.gamelogic.ChessParseError;
+import com.doan.covua.gamelogic.Move;
 import com.doan.covua.gamelogic.Piece;
 import com.doan.covua.gamelogic.Position;
 import com.doan.covua.gamelogic.TextIO;
+import com.doan.covua.gamelogic.UndoInfo;
 
 import android.content.Context;
 import android.content.res.Resources;
@@ -16,6 +18,7 @@ import android.graphics.Paint;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class ChessBoard extends View{
@@ -26,21 +29,20 @@ public class ChessBoard extends View{
 	
 	protected Paint darkPaint;
     protected Paint brightPaint;
+    protected Paint selectedSquarePaint;
+    
     protected int x0, y0, sqSize;
     int pieceXDelta, pieceYDelta;
     public boolean flipped;
-    
+    public int selectedSquare;
+    public boolean oneTouchMoves;
+    public float cursorX, cursorY;
+    public boolean cursorVisible;
     
     public ChessBoard(Context context, AttributeSet attr){
     	super(context, attr);
     	
     	pos = new Position();
-    	/*try {
-			pos = new Position(TextIO.readFEN(TextIO.startPosFEN));
-		} catch (ChessParseError e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
     	x0 = y0 = sqSize = 0;
 	    pieceXDelta = pieceYDelta = -1;
     	flipped = false;
@@ -49,6 +51,12 @@ public class ChessBoard extends View{
  	    darkPaint.setColor(Color.GRAY);
         brightPaint = new Paint();
         brightPaint.setColor(Color.WHITE);
+        
+        selectedSquarePaint = new Paint();
+        selectedSquarePaint.setStyle(Paint.Style.STROKE);
+        selectedSquarePaint.setAntiAlias(true);
+        selectedSquarePaint.setColor(Color.GREEN);
+        
     }
     
     protected int getWidth(int sqSize) { return sqSize * 8 + 4; }
@@ -81,17 +89,19 @@ public class ChessBoard extends View{
         y0 = (height - sqSize * 8) / 2;
 	}
 	
+	protected int getXFromSq(int sq) { return Position.getX(sq); }
+	protected int getYFromSq(int sq) { return Position.getY(sq); }
+
+	
 	@Override
 	protected void onDraw(Canvas canvas){
+		Log.v("in onDraw","rightnow");
+		boolean animActive = anim.updateState();
 		final int width = getWidth();
 		final int height = getHeight();
         sqSize = Math.min(getSqSizeW(width), getSqSizeH(height));
         computeOrigin(width, height);
        
-        //debug
-       // for(int i=0; i< 64; i++){
-       // 	Log.v("debug the board...","" + pos.squares[i]);
-       // }
         for(int x = 0; x < 8; x++)
         	for(int y = 0; y < 8; y++){
         		 final int xCrd = getXCrd(x);
@@ -100,23 +110,37 @@ public class ChessBoard extends View{
                  canvas.drawRect(xCrd, yCrd, xCrd+sqSize, yCrd+sqSize, paint);
                  
                  int sq = Position.getSquare(x, y);
-
-                 int p = pos.getPiece(sq);
-                 
-                 drawPiece(canvas, xCrd, yCrd, p);
+                 if (!animActive || !anim.squareHidden(sq)) {
+                	 int p = pos.getPiece(sq);
+                	
+                	 drawPiece(canvas, xCrd, yCrd, p);
+                 }
         	}
-       
+        
+        if(!animActive && (selectedSquare != -1)){
+        	
+        	int selX = getXFromSq(selectedSquare);
+            int selY = getYFromSq(selectedSquare);
+            selectedSquarePaint.setStrokeWidth(sqSize/(float)16);
+            
+            int x0 = getXCrd(selX);
+            int y0 = getYCrd(selY);
+          
+            canvas.drawRect(x0, y0, x0 + sqSize, y0 + sqSize, selectedSquarePaint);
+        }
+       anim.draw(canvas);
 	}
 	protected int getXCrd(int x) { return x0 + sqSize * (flipped ? 7 - x : x); }
     protected int getYCrd(int y) { return y0 + sqSize * (flipped ? y : 7 - y); }
-    
+    protected int getXSq(int xCrd) { int t = (xCrd - x0) / sqSize; return flipped ? 7 - t : t; }
+    protected int getYSq(int yCrd) { int t = (yCrd - y0) / sqSize; return flipped ? t : 7 - t; }
+
     protected final void drawPiece(Canvas canvas, int xCrd, int yCrd, int piece) {
-    	if(piece == 0)
+    	if(piece == Piece.EMPTY)
     		return;
 		//draw piece
 		Resources r = getResources();
 		Bitmap re_piece = BitmapFactory.decodeResource(r, 0);
-		Log.v("piec on draw piece...","" + piece);
 		switch(piece){
 		case Piece.EMPTY:
 			re_piece = null;
@@ -166,6 +190,10 @@ public class ChessBoard extends View{
      */
     final public void setPosition(Position pos) {
     	boolean doInvalidate = false;
+    	if(anim.paused == true){
+    		anim.paused = false;
+    		doInvalidate = true;
+    	}
     	if (!this.pos.equals(pos)) {
     		this.pos = new Position(pos);
     		doInvalidate = true;
@@ -173,8 +201,17 @@ public class ChessBoard extends View{
     	if (doInvalidate)
     		invalidate();
     }
-    
-private Handler handlerTimer = new Handler();
+    /**
+     * Set/clear the selected square.
+     * @param square The square to select, or -1 to clear selection.
+     */
+    final public void setSelection(int square) {
+        if (square != selectedSquare) {
+            selectedSquare = square;
+            invalidate();
+        }
+    }
+    private Handler handlerTimer = new Handler();
 	
 	final class AnimInfo {
 		AnimInfo() { startTime = -1; }
@@ -191,8 +228,8 @@ private Handler handlerTimer = new Handler();
 			return animActive();
 		}
 		private final boolean animActive() {
-			//if (paused || (startTime < 0) || (now >= stopTime) || (posHash != pos.zobristHash()))
-			//	return false;
+			if (paused || (startTime < 0) || (now >= stopTime))
+				return false;
 			return true;
 		}
 		public final boolean squareHidden(int sq) {
@@ -205,6 +242,9 @@ private Handler handlerTimer = new Handler();
 				return;
 			double animState = (now - startTime) / (double)(stopTime - startTime);
 			drawAnimPiece(canvas, piece2, from2, to2, animState);
+			//drawPiece(canvas, getXCrd(Position.getX(from1)), getYCrd(Position.getY(from1)), piece2);
+			//drawPiece(canvas, getXCrd(to1), getYCrd(to1), piece1);
+			//drawPiece(canvas, getXCrd(Position.getX(to1)), getYCrd(Position.getY(to1)), piece1);
 			drawAnimPiece(canvas, piece1, from1, to1, animState);
 			long now2 = System.currentTimeMillis();
 			long delay = 20 - (now2 - now);
@@ -216,18 +256,154 @@ private Handler handlerTimer = new Handler();
 					invalidate();
 				}
 	        }, delay);
+			invalidate();
 		}
 		private void drawAnimPiece(Canvas canvas, int piece, int from, int to, double animState) {
 			if (piece == Piece.EMPTY)
 				return;
+		
             final int xCrd1 = getXCrd(Position.getX(from));
             final int yCrd1 = getYCrd(Position.getY(from));
             final int xCrd2 = getXCrd(Position.getX(to));
             final int yCrd2 = getYCrd(Position.getY(to));
+        	
+        	
             final int xCrd = xCrd1 + (int)Math.round((xCrd2 - xCrd1) * animState);
             final int yCrd = yCrd1 + (int)Math.round((yCrd2 - yCrd1) * animState);
+            
         	drawPiece(canvas, xCrd, yCrd, piece);
 		}
 	}
 	AnimInfo anim = new AnimInfo();
+	
+	final private boolean myColor(int piece){
+		return (piece != Piece.EMPTY) && (Piece.isWhite(piece) == pos.whiteMove);
+	}
+	/**
+     * Compute the square corresponding to the coordinates of a mouse event.
+     * @param evt Details about the mouse event.
+     * @return The square corresponding to the mouse event, or -1 if outside board.
+     */
+    public int eventToSquare(MotionEvent evt) {
+    	int xCrd = (int)(evt.getX());
+        int yCrd = (int)(evt.getY());
+
+        int sq = -1;
+        if (sqSize > 0) {
+            int x = getXSq(xCrd);
+            int y = getYSq(yCrd);
+            if ((x >= 0) && (x < 8) && (y >= 0) && (y < 8)) {
+                sq = Position.getSquare(x, y);
+            }
+        }
+        return sq;
+    }
+	public Move mousePressed(int sq){
+		if(sq < 0)
+			return null;
+		if(selectedSquare != -1){
+			int p = pos.getPiece(selectedSquare);
+			if(!myColor(p))
+				setSelection(-1);
+		}
+		int p = pos.getPiece(sq);
+		if(selectedSquare != -1){
+			if(sq != selectedSquare){
+				if (!myColor(p)) {
+                    Move m = new Move(selectedSquare, sq, Piece.EMPTY);
+                    setSelection(sq);
+                    return m;
+                }
+			}
+			 setSelection(-1);
+		}else{
+			if(oneTouchMoves){
+				//do something
+				//..........
+			}
+        	if (myColor(p)) {
+        		setSelection(sq);
+        	}
+		}
+		return null;
+	}
+	/**
+	 * Set up move animation. The animation will start the next time setPosition is called.
+	 * @param sourcePos The source position for the animation.
+	 * @param move      The move leading to the target position.
+	 * @param forward   True if forward direction, false for undo move.
+	 */
+	public void setAnimMove(Position sourcePos, Move move, boolean forward) {
+		anim.startTime = -1;
+		if(forward){
+			// The animation will be played when pos == targetPos
+			Position targetPos = new Position(sourcePos);
+			UndoInfo ui = new UndoInfo();
+			targetPos.makeMove(move, ui);
+		}
+		int animTime; // Animation duration in milliseconds.
+		{
+			int dx = Position.getX(move.to) - Position.getX(move.from);
+			int dy = Position.getY(move.to) - Position.getY(move.from);
+			double dist = Math.sqrt(dx * dx + dy * dy);
+			double t = Math.sqrt(dist) * 100;
+			animTime = (int)Math.round(t);
+		}
+		if (animTime > 0) {
+			anim.startTime = System.currentTimeMillis();
+			anim.stopTime = anim.startTime + animTime;
+			anim.piece2 = Piece.EMPTY;
+			anim.from2 = -1;
+			anim.to2 = -1;
+			anim.hide2 = -1;
+			if (forward) {
+				int p = sourcePos.getPiece(move.from);
+				anim.piece1 = p;
+				anim.from1 = move.from;
+				anim.to1 = move.to;
+				anim.hide1 = anim.to1;
+				int p2 = sourcePos.getPiece(move.to);
+				if (p2 != Piece.EMPTY) { // capture
+					anim.piece2 = p2;
+					anim.from2 = move.to;
+					anim.to2 = move.to;
+				} else if ((p == Piece.WKING) || (p == Piece.BKING)) {
+					boolean wtm = Piece.isWhite(p);
+					if (move.to == move.from + 2) { // O-O
+						anim.piece2 = wtm ? Piece.WROOK : Piece.BROOK;
+						anim.from2 = move.to + 1;
+						anim.to2 = move.to - 1;
+						anim.hide2 = anim.to2;
+					} else if (move.to == move.from - 2) { // O-O-O
+						anim.piece2 = wtm ? Piece.WROOK : Piece.BROOK;
+						anim.from2 = move.to - 2;
+						anim.to2 = move.to + 1;
+						anim.hide2 = anim.to2;
+					}
+				}
+			} else {
+				int p = sourcePos.getPiece(move.from);
+				anim.piece1 = p;
+				if (move.promoteTo != Piece.EMPTY)
+					anim.piece1 = Piece.isWhite(anim.piece1) ? Piece.WPAWN : Piece.BPAWN;
+				anim.from1 = move.to;
+				anim.to1 = move.from;
+				anim.hide1 = anim.to1;
+				if ((p == Piece.WKING) || (p == Piece.BKING)) {
+					boolean wtm = Piece.isWhite(p);
+					if (move.to == move.from + 2) { // O-O
+						anim.piece2 = wtm ? Piece.WROOK : Piece.BROOK;
+						anim.from2 = move.to - 1;
+						anim.to2 = move.to + 1;
+						anim.hide2 = anim.to2;
+					} else if (move.to == move.from - 2) { // O-O-O
+						anim.piece2 = wtm ? Piece.WROOK : Piece.BROOK;
+						anim.from2 = move.to + 1;
+						anim.to2 = move.to - 2;
+						anim.hide2 = anim.to2;
+					}
+				}
+			}
+		}
+	}
 }
--- trunk/src/com/doan/covua/gamelogic/ChessController.java
+++ trunk/src/com/doan/covua/gamelogic/ChessController.java
@@ -1,28 +1,135 @@
 package com.doan.covua.gamelogic;
 
+import java.util.ArrayList;
+import java.util.List;
+import android.util.Log;
 import com.doan.covua.GUIInterface;
-import com.doan.covua.gamelogic.Position;;
+import com.doan.covua.GameMode;
+import com.doan.covua.gamelogic.Position;
+
 public class ChessController {
 
 	PgnToken.PgnTokenReceiver gameTextListener;
 	GUIInterface gui;
 	GameTree tree;
+	GameMode gameMode;
 	
 	public ChessController(GUIInterface gui, PgnToken.PgnTokenReceiver gamTextListener){
 		this.gui = gui;
 		this.gameTextListener = gamTextListener;
 	}
-	public final void newGame(){
+	public final void newGame(GameMode gameMode){
+		this.gameMode = gameMode;
 		tree = new GameTree(gameTextListener);
 	}
 	public final void startGame() {
-		updateUI();
+		setSelection();
+		updateGUI();
 	}
-	private final void updateUI(){
-		gui.setPosition(currPos(), null, null);
+	private final void updateGUI(){
+		
+		//updateMoveList();
+		//StringBuilder sb = new StringBuilder();
+		if(tree.currentNode != tree.rootNode){
+			//tree.goBack();
+			
+			//Position pos = currPos();
+			/*List<Move> prevVarList = tree.variations();
+			for (int i = 0; i < prevVarList.size(); i++) {
+				if (i > 0) sb.append(' ');
+				if (i == tree.currentNode.defaultChild)
+        			sb.append("<b>");
+        		sb.append(TextIO.moveToString(pos, prevVarList.get(i), false));
+        		if (i == tree.currentNode.defaultChild)
+        			sb.append("</b>");
+			}*/
+			//tree.goForward(-1);
+		}
+		gui.setPosition(currPos(), null, tree.variations());
 	}
 	/** get current position */
 	final Position currPos(){
 		return tree.currentPos;
 	}
+	/** get the last move */
+	public final Move getLastMove(){
+		return tree.currentNode.move;
+	}
+	final private void setSelection() {
+        Move m = getLastMove();
+        int sq = (m != null) ? m.to : -1;
+        gui.setSelection(sq);
+    }
+	/** True if human's turn to make a move. (True in analysis mode.) */
+	public final boolean humansTurn(){
+		return gameMode.humansTurn(currPos().whiteMove);
+	}
+	 private void setAnimMove(Position sourcePos, Move move, boolean forward) {
+	    	gui.setAnimMove(sourcePos, move, forward);
+		}
+	public final void makeHumanMove(Move m){
+		for(int i=0; i<64; i++){
+         	Log.v("make human move0000000000000","" + currPos().squares[i]);
+         }
+		Position oldPos = new Position(currPos());
+		if (doMove(m)) {
+			for(int i=0; i<64; i++)
+				Log.v("domove....","" + currPos().squares[i]);
+			 setAnimMove(oldPos, m, true);
+			 updateGUI();
+        } else {
+           gui.setSelection(-1);
+        }
+	}
+	 /**
+     * Move a piece from one square to another.
+     * @return True if the move was legal, false otherwise.
+     */
+	
+    final private boolean doMove(Move move) {
+    	//generate the moves 
+    	
+    	/*Position pos = currPos();
+    	ArrayList<Move> moves = new Move().pseudoLegalMoves(pos);
+        moves = Move.removeIllegal(pos, moves);
+        int promoteTo = move.promoteTo;
+        for (Move m : moves) {
+            if ((m.from == move.from) && (m.to == move.to)) {
+                if ((m.promoteTo != Piece.EMPTY) && (promoteTo == Piece.EMPTY)) {
+                	//promoteMove = m;
+                	//gui.requestPromotePiece();
+                	
+                	return false;
+                }
+                if (m.promoteTo == promoteTo) {
+                	
+                    //String strMove = TextIO.moveToString(pos, m, false);
+                   
+                    //processString(strMove);
+                    //addToGameTree(m,"");
+                	resetPos(move);
+                    return true;
+                }
+            }
+        }*/
+    	
+        resetPos(move);
+        return true;
+        //return false;
+    }
+    /** reset squares position */
+    /** temp method */
+    final private Position resetPos(Move move){
+    	Position pos = currPos();
+    	int pos_temp = pos.squares[move.from];
+    	pos.squares[move.from] = 0;
+    	
+    	Log.v("square to","" + move.to);
+    	
+    	pos.squares[move.to] = pos_temp;
+    	Position pos1 = new Position(pos);
+    	return pos1;
+    }
+   
 }
+
